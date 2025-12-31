96cc1fe990c3c5ddd32666a225812a26c093c930
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-12-31 13:11:57 -0300

    feat: add ingredient form with name, price, quantity, and unit fields to the AddIngredientModal.

--- src/pages/Ingredients/components/AddIngredientModal.tsx
+++ src/pages/Ingredients/components/AddIngredientModal.tsx
@@ -5,24 +5,63 @@ export const AddIngredientModal = ({
   isOpenModal: boolean;
   setIsOpenModal: (isOpenModal: boolean) => void;
 }) => {
-  // Función para cerrar el modal
   const closeModal = () => setIsOpenModal(false);
 
   return (
     <div>
       <div className={`modal ${isOpenModal ? "modal-open" : ""}`} role="dialog">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">¡Hola!</h3>
-          <p className="py-4">
-            Este modal está controlado por el estado de React.
-          </p>
+          <h3 className="font-bold text-lg mb-4">Agregar ingrediente</h3>
 
-          <div className="modal-action">
-            {/* Botón interno para cerrar */}
-            <button className="btn" onClick={closeModal}>
-              Cerrar
-            </button>
-          </div>
+          <form>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Nombre</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered"
+                placeholder="Huevos, leche, etc."
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Precio</span>
+              </label>
+              <input
+                type="number"
+                className="input input-bordered"
+                placeholder="250"
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Cantidad</span>
+              </label>
+              <input
+                type="number"
+                className="input input-bordered"
+                placeholder="1"
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Unidad</span>
+              </label>
+              <select className="select select-bordered">
+                <option value="" disabled selected>
+                  --Selecciona un tipo de unidad--
+                </option>
+                <option value="unidad">Unidad/es</option>
+                <option value="gramos">Gramos</option>
+                <option value="kilos">Kilos</option>
+                <option value="litros">Litros</option>
+              </select>
+            </div>
+            <div className="flex justify-center">
+              <button className="btn btn-primary">Agregar ingrediente</button>
+            </div>
+          </form>
         </div>
 
         {/* Backdrop: Al hacer clic aquí, se cierra el modal */}
